bf2aafc8def868d8c424ed8dd4d86a6eb87f1400
vonroga <ryanroga@gmail.com>
2022-09-27 15:02:30 -0700

    Create TopBar.svelte

---
+++ src/lib/components/TopBar.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-lg font-bold p-4">Roga's Pokemon Map</h1>
+
+<style>
+  h1 {
+    display: table-cell;
+    height: 10vh;
+    vertical-align: middle;
+    padding-left: 2rem;
+  }
+</style>
