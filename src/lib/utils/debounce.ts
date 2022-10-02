2553c0d46a9b915943895e909f226c09d1eb3b52
vonroga <ryanroga@gmail.com>
2022-10-02 13:16:27 -0700

    Update debounce.ts

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -1,7 +1,7 @@
+let timeout: NodeJS.Timeout
 export default function debounce<
   F extends (...args: Parameters<F>) => ReturnType<F>
->(func: F, waitFor: number = 30): (...args: Parameters<F>) => void {
-  let timeout: NodeJS.Timeout
+>(func: F, waitFor: number = 300): (...args: Parameters<F>) => void {
   return (...args: Parameters<F>): void => {
     clearTimeout(timeout)
     timeout = setTimeout(() => func(...args), waitFor)
