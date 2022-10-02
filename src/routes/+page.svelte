65a3ee39302b3672ad1ebdc13da20447ecd5e801
vonroga <ryanroga@gmail.com>
2022-10-02 09:06:43 -0700

    Update +page.svelte
    Now loading pokemon on main page load.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,13 @@
   import TopBar from '$lib/components/TopBar.svelte'
   import ActionBar from '$lib/components/ActionBar.svelte'
   import Map from '$lib/components/Map.svelte'
+  import { loadPokemon } from '$lib/stores/pokemon'
+  loadPokemon()
 </script>
+
+<svelte:head>
+  <title>Roga's Pokemon Map</title>
+</svelte:head>
 
 <TopBar />
 <Map />
