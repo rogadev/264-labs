24faecab3138f0f22e7578a7be6bd5671e6aedae
vonroga <ryanroga@gmail.com>
2022-10-02 14:24:24 -0700

    Create Trainer.ts

---
+++ src/lib/classes/Trainer.ts
@@ -0,0 +1,16 @@
+import type { Pokemon } from "$lib/types/types"
+
+class Trainer {
+  name: 'Roga',
+  location: [0, 0],
+  icon: {
+    iconUrl: 'https://play.pokemonshowdown.com/sprites/trainers/ash-kalos.png',
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+  },
+  pokemon: Pokemon[],
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
