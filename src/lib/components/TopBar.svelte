0541bc45573814cc459979892f4362075749a586
vonroga <ryanroga@gmail.com>
2022-10-02 15:06:47 -0700

    Update TopBar.svelte

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -5,6 +5,6 @@
 <div
   class="absolute top-0 z-10 text-white w-[100vw] h-[10vh] flex justify-between items-center px-16"
 >
-  <h1 class="text-lg font-bold">Roga's Pokemon Map</h1>
+  <h1 class="text-lg font-bold">Roga's Pokemon Game</h1>
   <h2 class="text-md font-bold">{$weather}</h2>
 </div>
