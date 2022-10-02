c7296407666fc892ca1f1dce5c6a31a6b79335c2
vonroga <ryanroga@gmail.com>
2022-10-02 14:51:49 -0700

    Adds api/add-node on server side

---
+++ src/routes/api/add-node/+server.ts
@@ -0,0 +1,16 @@
+import nodeData from '$lib/data/nodes.json'
+import fs from 'fs'
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+  const body = await request.json()
+  const location = body.location
+  const prevNodeData = nodeData
+  const newNodes = [...prevNodeData, location]
+  fs.writeFileSync('src/lib/data/nodes.json', JSON.stringify(newNodes, null, 2))
+  return {
+    status: 200,
+    body: {
+      message: 'Node added',
+    },
+  }
+}
