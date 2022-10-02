5fe734b73c0e38d9da74237055d15e76c7e83da3
vonroga <ryanroga@gmail.com>
2022-10-02 13:13:44 -0700

    Create trainer.ts

---
+++ src/lib/stores/trainer.ts
@@ -0,0 +1,18 @@
+import type { Trainer } from '$lib/types/types'
+import { writable, type Writable } from 'svelte/store'
+
+let loaded = false
+
+/**
+ * Trainer Store
+ */
+export const trainer: Writable<Trainer> = writable({
+  name: 'Roga',
+  location: [0, 0],
+  icon: {
+    iconUrl: 'https://play.pokemonshowdown.com/sprites/trainers/ash-kalos.png',
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+  },
+  pokemon: [],
+})
