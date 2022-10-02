22eb1ef11bc3c00cfffcfc63f68aff0573355efd
vonroga <ryanroga@gmail.com>
2022-10-02 11:58:26 -0700

    Update weather.ts

--- src/lib/stores/weather.ts
+++ src/lib/stores/weather.ts
@@ -2,13 +2,28 @@ import { writable, type Writable } from 'svelte/store'
 import chooseRandom from '$lib/utils/chooseRandom'
 import type { Weather } from '$lib/types/types'
 
-const weather: Writable<MapNode[]> = writable(initWeather())
+let loaded = false
+const weatherChoices: Weather = [
+  'Sunny',
+  'Cloudy',
+  'Rainy',
+  'Snowy',
+  'Windy',
+  'Foggy',
+]
 
-function initWeather() {
-  const choices: Weather = ['Sunny', 'Raining', 'Snowing', 'Foggy', 'Windy']
-  const weather = chooseRandom(choices)
-  console.log('Initializing weather:', weather)
-  return weather
+export const weather: Writable<Weather> = writable(null)
+
+export function initWeather() {
+  if (loaded) return
+  const initialWeather = chooseRandom(weatherChoices)
+  console.info('Initializing weather:', initialWeather)
+  weather.set(initialWeather)
+  loaded = true
 }
 
-export default nodes
+export function changeWeather() {
+  const newWeather = chooseRandom(weatherChoices)
+  console.info('Weather has changed:', newWeather)
+  weather.set(newWeather)
+}
