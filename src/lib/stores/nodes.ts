a427b8e0497a01595d987d0dbb80b8f01b705719
vonroga <ryanroga@gmail.com>
2022-10-02 14:51:10 -0700

    Update nodes.ts

--- src/lib/stores/nodes.ts
+++ src/lib/stores/nodes.ts
@@ -1,5 +1,22 @@
 import { writable, type Writable } from 'svelte/store'
-import nodeList from '$lib/data/nodes'
+import nodeData from '$lib/data/nodes.json'
+import fs from 'fs'
 
-const nodes: Writable<MapNode[]> = writable(nodeList)
-export default nodes
+const radius = 8
+const nodeList = nodeData.map((location) => {
+  return {
+    location,
+    radius,
+  }
+})
+
+export const nodes: Writable<MapNode[]> = writable(nodeList)
+
+export function addNode(location: [number, number]) {
+  const prevNodeData = nodeData
+  const newNodes = [...prevNodeData, location]
+  fs.writeFileSync('src/lib/data/nodes.json', JSON.stringify(newNodes, null, 2))
+  nodes.update((n) => {
+    return [...n, { location, radius }]
+  })
+}
